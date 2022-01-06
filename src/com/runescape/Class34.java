a58b8bcdaaeef55fcdbba9ca44b668f58bfa236e
Pazaz <pazaz@itspazaz.com>
2022-01-05 22:22:05 -0500

    Class36 -> Hashtable

--- src/com/runescape/Class34.java
+++ src/com/runescape/Class34.java
@@ -4,7 +4,7 @@ public class Class34 {
 
     public Class34(byte byte0, int i) {
         aBoolean590 = false;
-        aClass36_594 = new Class36(9, 1024);
+        hashtable = new Hashtable(1024);
         aClass29_595 = new Class29(anInt591);
         anInt592 = i;
         if (byte0 != 0) {
@@ -15,7 +15,7 @@ public class Class34 {
     }
 
     public Class38_Sub2 method341(long l) {
-        Class38_Sub2 class38_sub2 = (Class38_Sub2) aClass36_594.method344(l);
+        Class38_Sub2 class38_sub2 = (Class38_Sub2) hashtable.get(l);
         if (class38_sub2 != null)
             aClass29_595.method275(class38_sub2);
         return class38_sub2;
@@ -29,7 +29,7 @@ public class Class34 {
         } else {
             anInt593--;
         }
-        aClass36_594.method345(l, -566, class38_sub2);
+        hashtable.put(l, class38_sub2);
         if (i < 6 || i > 6)
             aBoolean590 = !aBoolean590;
         aClass29_595.method275(class38_sub2);
@@ -52,7 +52,7 @@ public class Class34 {
     public static int anInt591 = 5;
     public int anInt592;
     public int anInt593;
-    public Class36 aClass36_594;
+    public Hashtable hashtable;
     public Class29 aClass29_595;
 
 }
