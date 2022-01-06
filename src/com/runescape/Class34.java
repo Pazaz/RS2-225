25c45f30348064870acab747f29a0ed1db542b1c
Pazaz <pazaz@itspazaz.com>
2022-01-05 23:43:16 -0500

    Class29 -> Stack

--- src/com/runescape/Class34.java
+++ src/com/runescape/Class34.java
@@ -5,7 +5,7 @@ public class Class34 {
     public Class34(byte byte0, int i) {
         aBoolean590 = false;
         hashtable = new Hashtable(1024);
-        aClass29_595 = new Class29(anInt591);
+        stack = new Stack();
         anInt592 = i;
         if (byte0 != 0) {
             for (int j = 1; j > 0; j++)
@@ -17,13 +17,13 @@ public class Class34 {
     public CacheableNode method341(long l) {
         CacheableNode cacheableNode = (CacheableNode) hashtable.get(l);
         if (cacheableNode != null)
-            aClass29_595.method275(cacheableNode);
+            stack.push(cacheableNode);
         return cacheableNode;
     }
 
     public void method342(int i, long l, CacheableNode cacheableNode) {
         if (anInt593 == 0) {
-            CacheableNode cacheableNode_1 = aClass29_595.method276();
+            CacheableNode cacheableNode_1 = stack.pop();
             cacheableNode_1.unlink();
             cacheableNode_1.uncache();
         } else {
@@ -32,12 +32,12 @@ public class Class34 {
         hashtable.put(l, cacheableNode);
         if (i < 6 || i > 6)
             aBoolean590 = !aBoolean590;
-        aClass29_595.method275(cacheableNode);
+        stack.push(cacheableNode);
     }
 
     public void method343() {
         do {
-            CacheableNode cacheableNode = aClass29_595.method276();
+            CacheableNode cacheableNode = stack.pop();
             if (cacheableNode != null) {
                 cacheableNode.unlink();
                 cacheableNode.uncache();
@@ -53,6 +53,6 @@ public class Class34 {
     public int anInt592;
     public int anInt593;
     public Hashtable hashtable;
-    public Class29 aClass29_595;
+    public Stack stack;
 
 }
