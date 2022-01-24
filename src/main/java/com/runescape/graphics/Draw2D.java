520d2894b50761bf43283ba3ad0ad96f1522ceae
Pazaz <pazaz@itspazaz.com>
2022-01-24 18:44:16 -0500

    Finished refactoring local variables in Draw2D

--- src/main/java/com/runescape/graphics/Draw2D.java
+++ src/main/java/com/runescape/graphics/Draw2D.java
@@ -20,87 +20,86 @@ public class Draw2D extends CacheableNode {
         centerX = right / 2;
     }
 
-    public static void setBounds(int i, int j, int k, int i1) {
-        if (i1 < 0)
-            i1 = 0;
-        if (j < 0)
-            j = 0;
-        if (k > width)
-            k = width;
-        if (i > height)
-            i = height;
-        left = i1;
-        top = j;
-        right = k;
-        bottom = i;
+    public static void setBounds(int y1, int y0, int x1, int x0) {
+        if (x0 < 0)
+            x0 = 0;
+        if (y0 < 0)
+            y0 = 0;
+        if (x1 > width)
+            x1 = width;
+        if (y1 > height)
+            y1 = height;
+        left = x0;
+        top = y0;
+        right = x1;
+        bottom = y1;
         rightX = right - 1;
         centerX = right / 2;
         centerY = bottom / 2;
     }
 
     public static void clear() {
-        int j = width * height;
-        for (int k = 0; k < j; k++) {
-            dest[k] = 0;
+        int len = width * height;
+        for (int i = 0; i < len; i++) {
+            dest[i] = 0;
         }
     }
 
-    public static void fillRect(int i, int j, int k, int l, int i1) {
-        if (j < left) {
-            l -= left - j;
-            j = left;
+    public static void fillRect(int y, int x, int rgb, int w, int h) {
+        if (x < left) {
+            w -= left - x;
+            x = left;
         }
-        if (i < top) {
-            i1 -= top - i;
-            i = top;
+        if (y < top) {
+            h -= top - y;
+            y = top;
         }
-        if (j + l > right)
-            l = right - j;
-        if (i + i1 > bottom)
-            i1 = bottom - i;
-        int j1 = width - l;
-        int k1 = j + i * width;
-        for (int l1 = -i1; l1 < 0; l1++) {
-            for (int i2 = -l; i2 < 0; i2++)
-                dest[k1++] = k;
-            k1 += j1;
+        if (x + w > right)
+            w = right - x;
+        if (y + h > bottom)
+            h = bottom - y;
+        int stride = width - w;
+        int off = x + y * width;
+        for (int i = -h; i < 0; i++) {
+            for (int j = -w; j < 0; j++)
+                dest[off++] = rgb;
+            off += stride;
         }
     }
 
-    public static void drawRect(int j, int k, int l, int i1, int j1) {
-        drawHorizontalLine(k, i1, j1, j);
-        drawHorizontalLine(k, (i1 + l) - 1, j1, j);
-        drawVerticalLine(k, i1, l, j);
-        drawVerticalLine(k, i1, l, (j + j1) - 1);
+    public static void drawRect(int x, int rgb, int h, int y, int len) {
+        drawHorizontalLine(rgb, y, len, x);
+        drawHorizontalLine(rgb, (y + h) - 1, len, x);
+        drawVerticalLine(rgb, y, h, x);
+        drawVerticalLine(rgb, y, h, (x + len) - 1);
     }
 
-    public static void drawHorizontalLine(int i, int k, int l, int i1) {
-        if (k < top || k >= bottom)
+    public static void drawHorizontalLine(int rgb, int y, int len, int x) {
+        if (y < top || y >= bottom)
             return;
-        if (i1 < left) {
-            l -= left - i1;
-            i1 = left;
+        if (x < left) {
+            len -= left - x;
+            x = left;
         }
-        if (i1 + l > right)
-            l = right - i1;
-        int j1 = i1 + k * width;
-        for (int k1 = 0; k1 < l; k1++)
-            dest[j1 + k1] = i;
+        if (x + len > right)
+            len = right - x;
+        int off = x + y * width;
+        for (int i = 0; i < len; i++)
+            dest[off + i] = rgb;
     }
 
-    public static void drawVerticalLine(int i, int k, int l, int i1) {
-        if (i1 < left || i1 >= right)
+    public static void drawVerticalLine(int rgb, int y, int len, int x) {
+        if (x < left || x >= right)
             return;
-        if (k < top) {
-            l -= top - k;
-            k = top;
+        if (y < top) {
+            len -= top - y;
+            y = top;
         }
-        if (k + l > bottom)
-            l = bottom - k;
-        int j1 = i1 + k * width;
-        for (int k1 = 0; k1 < l; k1++)
-            dest[j1 + k1 * width] = i;
-
+        if (y + len > bottom)
+            len = bottom - y;
+        int off = x + y * width;
+        for (int i = 0; i < len; i++)
+            dest[off + i * width] = rgb;
     }
 
     public Draw2D() {
