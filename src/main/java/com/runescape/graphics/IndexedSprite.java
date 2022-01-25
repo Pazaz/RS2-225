7912b9cf860beb46c04a8cbca3814743d609d17f
Pazaz <pazaz@itspazaz.com>
2022-01-24 19:04:32 -0500

    Finished refactoring local variables in IndexedSprite

--- src/main/java/com/runescape/graphics/IndexedSprite.java
+++ src/main/java/com/runescape/graphics/IndexedSprite.java
@@ -5,18 +5,20 @@ import com.runescape.util.Buffer;
 
 public class IndexedSprite extends Draw2D {
 
-    public IndexedSprite(FileArchive fileArchive, String s, int i) {
-        Buffer data = new Buffer(fileArchive.read(s + ".dat", null));
-        Buffer idx = new Buffer(fileArchive.read("index.dat", null));
+    public IndexedSprite(FileArchive archive, String name, int index) {
+        Buffer data = new Buffer(archive.read(name + ".dat", null));
+        Buffer idx = new Buffer(archive.read("index.dat", null));
+
         idx.offset = data.readWord();
         clipWidth = idx.readWord();
         clipHeight = idx.readWord();
-        int j = idx.readByte();
-        palette = new int[j];
-        for (int k = 0; k < j - 1; k++)
-            palette[k + 1] = idx.readSWord();
 
-        for (int l = 0; l < i; l++) {
+        palette = new int[idx.readByte()];
+        for (int n = 0; n < palette.length - 1; n++) {
+            palette[n + 1] = idx.readSWord();
+        }
+
+        for (int n = 0; n < index; n++) {
             idx.offset += 2;
             data.offset += idx.readWord() * idx.readWord();
             idx.offset++;
@@ -26,19 +28,19 @@ public class IndexedSprite extends Draw2D {
         clipY = idx.readByte();
         width = idx.readWord();
         height = idx.readWord();
-        int i1 = idx.readByte();
-        int j1 = width * height;
-        pixels = new byte[j1];
-        if (i1 == 0) {
-            for (int k1 = 0; k1 < j1; k1++)
-                pixels[k1] = data.readByteSigned();
 
-            return;
-        }
-        if (i1 == 1) {
-            for (int l1 = 0; l1 < width; l1++) {
-                for (int i2 = 0; i2 < height; i2++)
-                    pixels[l1 + i2 * width] = data.readByteSigned();
+        int type = idx.readByte();
+        pixels = new byte[width * height];
+
+        if (type == 0) {
+            for (int n = 0; n < pixels.length; n++) {
+                pixels[n] = data.readByteSigned();
+            }
+        } else if (type == 1) {
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    pixels[x + y * width] = data.readByteSigned();
+                }
             }
         }
     }
@@ -47,14 +49,15 @@ public class IndexedSprite extends Draw2D {
         clipWidth /= 2;
         clipHeight /= 2;
 
-        byte[] abyte0 = new byte[clipWidth * clipHeight];
+        byte[] newPixels = new byte[clipWidth * clipHeight];
         int i = 0;
         for (int j = 0; j < height; j++) {
-            for (int k = 0; k < width; k++)
-                abyte0[(k + clipX >> 1) + (j + clipY >> 1) * clipWidth] = pixels[i++];
+            for (int k = 0; k < width; k++) {
+                newPixels[(k + clipX >> 1) + (j + clipY >> 1) * clipWidth] = pixels[i++];
+            }
         }
 
-        pixels = abyte0;
+        pixels = newPixels;
         width = clipWidth;
         height = clipHeight;
         clipX = 0;
@@ -62,16 +65,19 @@ public class IndexedSprite extends Draw2D {
     }
 
     public void crop() {
-        if (width == clipWidth && height == clipHeight)
+        if (width == clipWidth && height == clipHeight) {
             return;
-        byte[] abyte0 = new byte[clipWidth * clipHeight];
-        int j = 0;
-        for (int k = 0; k < height; k++) {
-            for (int l = 0; l < width; l++)
-                abyte0[l + clipX + (k + clipY) * clipWidth] = pixels[j++];
         }
 
-        pixels = abyte0;
+        byte[] newPixels = new byte[clipWidth * clipHeight];
+        int off = 0;
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                newPixels[x + clipX + (y + clipY) * clipWidth] = pixels[off++];
+            }
+        }
+
+        pixels = newPixels;
         width = clipWidth;
         height = clipHeight;
         clipX = 0;
@@ -79,131 +85,143 @@ public class IndexedSprite extends Draw2D {
     }
 
     public void flipHorizontally() {
-        byte[] abyte0 = new byte[width * height];
-        int j = 0;
-        for (int k = 0; k < height; k++) {
-            for (int l = width - 1; l >= 0; l--)
-                abyte0[j++] = pixels[l + k * width];
+        byte[] flipped = new byte[width * height];
+        int off = 0;
+
+        for (int y = 0; y < height; y++) {
+            for (int x = width - 1; x >= 0; x--) {
+                flipped[off++] = pixels[x + y * width];
+            }
         }
 
-        pixels = abyte0;
+        pixels = flipped;
         clipX = clipWidth - width - clipX;
     }
 
     public void flipVertically() {
-        byte[] abyte0 = new byte[width * height];
-        int j = 0;
-        for (int k = height - 1; k >= 0; k--) {
-            for (int l = 0; l < width; l++)
-                abyte0[j++] = pixels[l + k * width];
+        byte[] flipped = new byte[width * height];
+
+        int off = 0;
+        for (int y = height - 1; y >= 0; y--) {
+            for (int x = 0; x < width; x++) {
+                flipped[off++] = pixels[x + y * width];
+            }
         }
 
-        pixels = abyte0;
+        pixels = flipped;
         clipY = clipHeight - height - clipY;
     }
 
-    public void translate(int i, int j, int k) {
-        for (int l = 0; l < palette.length; l++) {
-            int i1 = palette[l] >> 16 & 0xff;
-            i1 += i;
-            if (i1 < 0)
-                i1 = 0;
-            else if (i1 > 255)
-                i1 = 255;
-            int k1 = palette[l] >> 8 & 0xff;
-            k1 += j;
-            if (k1 < 0)
-                k1 = 0;
-            else if (k1 > 255)
-                k1 = 255;
-            int l1 = palette[l] & 0xff;
-            l1 += k;
-            if (l1 < 0)
-                l1 = 0;
-            else if (l1 > 255)
-                l1 = 255;
-            palette[l] = (i1 << 16) + (k1 << 8) + l1;
+    public void translate(int r, int g, int b) {
+        for (int i = 0; i < palette.length; i++) {
+            int red = palette[i] >> 16 & 0xff;
+            red += r;
+            if (red < 0)
+                red = 0;
+            else if (red > 255)
+                red = 255;
+            int green = palette[i] >> 8 & 0xff;
+            green += g;
+            if (green < 0)
+                green = 0;
+            else if (green > 255)
+                green = 255;
+            int blue = palette[i] & 0xff;
+            blue += b;
+            if (blue < 0)
+                blue = 0;
+            else if (blue > 255)
+                blue = 255;
+            palette[i] = (red << 16) + (green << 8) + blue;
         }
     }
 
-    public void draw(int i, int j) {
-        j += clipX;
-        i += clipY;
-        int k = j + i * Draw2D.width;
-        int l = 0;
-        int i1 = height;
-        int j1 = width;
-        int k1 = Draw2D.width - j1;
-        int l1 = 0;
-        if (i < Draw2D.top) {
-            int i2 = Draw2D.top - i;
-            i1 -= i2;
-            i = Draw2D.top;
-            l += i2 * j1;
-            k += i2 * Draw2D.width;
+    public void draw(int y, int x) {
+        x += clipX;
+        y += clipY;
+
+        int dstOff = x + y * Draw2D.width;
+        int srcOff = 0;
+
+        int h = height;
+        int w = width;
+        
+        int dstStep = Draw2D.width - w;
+        int srcStep = 0;
+        
+        if (y < Draw2D.top) {
+            int trim = Draw2D.top - y;
+            h -= trim;
+            y = Draw2D.top;
+            srcOff += trim * w;
+            dstOff += trim * Draw2D.width;
         }
-        if (i + i1 > Draw2D.bottom)
-            i1 -= (i + i1) - Draw2D.bottom;
-        if (j < Draw2D.left) {
-            int j2 = Draw2D.left - j;
-            j1 -= j2;
-            j = Draw2D.left;
-            l += j2;
-            k += j2;
-            l1 += j2;
-            k1 += j2;
+
+        if (y + h > Draw2D.bottom) {
+            h -= (y + h) - Draw2D.bottom;
         }
-        if (j + j1 > Draw2D.right) {
-            int k2 = (j + j1) - Draw2D.right;
-            j1 -= k2;
-            l1 += k2;
-            k1 += k2;
+
+        if (x < Draw2D.left) {
+            int trim = Draw2D.left - x;
+            w -= trim;
+            x = Draw2D.left;
+            srcOff += trim;
+            dstOff += trim;
+            srcStep += trim;
+            dstStep += trim;
         }
-        if (j1 > 0 && i1 > 0) {
-            copyImage(Draw2D.dest, l, l1, pixels, i1, 0, j1, k, k1, palette);
+
+        if (x + w > Draw2D.right) {
+            int trim = (x + w) - Draw2D.right;
+            w -= trim;
+            srcStep += trim;
+            dstStep += trim;
+        }
+
+        if (w > 0 && h > 0) {
+            copyImage(Draw2D.dest, srcOff, srcStep, pixels, h, w, dstOff, dstStep, palette);
         }
     }
 
-    public void copyImage(int[] ai, int i, int j, byte[] abyte0, int k, int l, int i1,
-                          int j1, int k1, int[] ai1) {
-        int l1 = -(i1 >> 2);
-        i1 = -(i1 & 3);
-        if (l != 0)
-            return;
-        for (int i2 = -k; i2 < 0; i2++) {
-            for (int j2 = l1; j2 < 0; j2++) {
-                byte byte0 = abyte0[i++];
-                if (byte0 != 0)
-                    ai[j1++] = ai1[byte0 & 0xff];
+    public void copyImage(int[] dst, int srcOff, int srcStep, byte[] src, int h, int w,
+                          int dstOff, int dstStep, int[] palette) {
+        int hw = -(w >> 2);
+        w = -(w & 3);
+        
+        for (int y = -h; y < 0; y++) {
+            for (int x = hw; x < 0; x++) {
+                byte p = src[srcOff++];
+                if (p != 0)
+                    dst[dstOff++] = palette[p & 0xff];
                 else
-                    j1++;
-                byte0 = abyte0[i++];
-                if (byte0 != 0)
-                    ai[j1++] = ai1[byte0 & 0xff];
+                    dstOff++;
+                p = src[srcOff++];
+                if (p != 0)
+                    dst[dstOff++] = palette[p & 0xff];
                 else
-                    j1++;
-                byte0 = abyte0[i++];
-                if (byte0 != 0)
-                    ai[j1++] = ai1[byte0 & 0xff];
+                    dstOff++;
+                p = src[srcOff++];
+                if (p != 0)
+                    dst[dstOff++] = palette[p & 0xff];
                 else
-                    j1++;
-                byte0 = abyte0[i++];
-                if (byte0 != 0)
-                    ai[j1++] = ai1[byte0 & 0xff];
+                    dstOff++;
+                p = src[srcOff++];
+                if (p != 0)
+                    dst[dstOff++] = palette[p & 0xff];
                 else
-                    j1++;
+                    dstOff++;
             }
 
-            for (int k2 = i1; k2 < 0; k2++) {
-                byte byte1 = abyte0[i++];
-                if (byte1 != 0)
-                    ai[j1++] = ai1[byte1 & 0xff];
+            for (int x = w; x < 0; x++) {
+                byte p = src[srcOff++];
+                if (p != 0)
+                    dst[dstOff++] = palette[p & 0xff];
                 else
-                    j1++;
+                    dstOff++;
             }
 
-            j1 += k1;
-            i += j;
+            dstOff += dstStep;
+            srcOff += srcStep;
         }
     }
 
