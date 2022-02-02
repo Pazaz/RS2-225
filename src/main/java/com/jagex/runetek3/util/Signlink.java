3998c4104eb2028d40ba3c2c60d31055ad460779
Pazaz <pazaz@itspazaz.com>
2022-02-02 03:51:12 -0500

    Clean up Signlink

--- src/main/java/com/jagex/runetek3/util/Signlink.java
+++ src/main/java/com/jagex/runetek3/util/Signlink.java
@@ -12,8 +12,9 @@ import java.net.URL;
 public class Signlink
     implements Runnable {
 
-    public static void startpriv(InetAddress inetaddress) {
+    public static void startpriv(InetAddress address) {
         threadliveid = (int) (Math.random() * 99999999D);
+
         if (active) {
             try {
                 Thread.sleep(500L);
@@ -21,16 +22,13 @@ public class Signlink
             }
             active = false;
         }
-        socketreq = 0;
-        threadreq = null;
-        dnsreq = null;
-        loadreq = null;
-        savereq = null;
-        urlreq = null;
-        socketip = inetaddress;
+
+        socketip = address;
+
         Thread thread = new Thread(new Signlink());
         thread.setDaemon(true);
         thread.start();
+
         while (!active) {
             try {
                 Thread.sleep(50L);
@@ -133,6 +131,7 @@ public class Signlink
             sequencer.setLoopCount(Sequencer.LOOP_CONTINUOUSLY);
             sequencer.start();
         } catch (Exception ex) {
+            System.out.println(file);
             ex.printStackTrace();
         }
     }
@@ -206,40 +205,46 @@ public class Signlink
     }
 
     public static String findcachedir() {
-        String[] as = {
+        String[] directories = {
             "c:/windows/", "c:/winnt/", "d:/windows/", "d:/winnt/", "e:/windows/", "e:/winnt/", "f:/windows/", "f:/winnt/", "c:/", "~/",
             "/tmp/", ""
         };
-        String s = ".file_store_32";
-        for (int i = 0; i < as.length; i++)
+        String name = ".file_store_32";
+
+        for (int i = 0; i < directories.length; i++) {
             try {
-                String s1 = as[i];
-                if (s1.length() > 0) {
-                    File file = new File(s1);
-                    if (!file.exists())
+                String dir = directories[i];
+                if (dir.length() > 0) {
+                    File file = new File(dir);
+                    if (!file.exists()) {
                         continue;
+                    }
                 }
-                File file1 = new File(s1 + s);
-                if (file1.exists() || file1.mkdir())
-                    return s1 + s + "/";
+
+                File file1 = new File(dir + name);
+                if (file1.exists() || file1.mkdir()) {
+                    return dir + name + "/";
+                }
             } catch (Exception _ex) {
             }
+        }
 
         return null;
     }
 
-    public static int getuid(String s) {
+    public static int getuid(String dir) {
         try {
-            File file = new File(s + "uid.dat");
+            File file = new File(dir + "uid.dat");
             if (!file.exists() || file.length() < 4L) {
-                DataOutputStream dataoutputstream = new DataOutputStream(new FileOutputStream(s + "uid.dat"));
+                DataOutputStream dataoutputstream = new DataOutputStream(new FileOutputStream(dir + "uid.dat"));
                 dataoutputstream.writeInt((int) (Math.random() * 99999999D));
                 dataoutputstream.close();
             }
         } catch (Exception _ex) {
         }
+
         try {
-            DataInputStream datainputstream = new DataInputStream(new FileInputStream(s + "uid.dat"));
+            DataInputStream datainputstream = new DataInputStream(new FileInputStream(dir + "uid.dat"));
             int i = datainputstream.readInt();
             datainputstream.close();
             return i + 1;
@@ -248,85 +253,97 @@ public class Signlink
         }
     }
 
-    public static long gethash(String s) {
-        s = s.trim();
+    public static long gethash(String str) {
+        str = str.trim();
         long l = 0L;
-        for (int i = 0; i < s.length() && i < 12; i++) {
-            char c = s.charAt(i);
+        for (int i = 0; i < str.length() && i < 12; i++) {
+            char c = str.charAt(i);
             l *= 37L;
-            if (c >= 'A' && c <= 'Z')
+            if (c >= 'A' && c <= 'Z') {
                 l += (1 + c) - 65;
-            else if (c >= 'a' && c <= 'z')
+            } else if (c >= 'a' && c <= 'z') {
                 l += (1 + c) - 97;
-            else if (c >= '0' && c <= '9')
+            } else if (c >= '0' && c <= '9') {
                 l += (27 + c) - 48;
+            }
         }
 
         return l;
     }
 
-    public static void looprate(int i) {
-        looprate = i;
+    public static void looprate(int rate) {
+        looprate = rate;
     }
 
-    public static synchronized byte[] cacheload(String s) {
-        if (!active)
+    public static synchronized byte[] cacheload(String name) {
+        if (!active) {
             return null;
-        for (loadreq = String.valueOf(gethash(s)); loadreq != null; )
+        }
+
+        for (loadreq = String.valueOf(gethash(name)); loadreq != null; ) {
             try {
                 Thread.sleep(1L);
             } catch (Exception _ex) {
             }
+        }
 
         return loadbuf;
     }
 
-    public static synchronized void cachesave(String s, byte[] abyte0) {
-        if (!active)
+    public static synchronized void cachesave(String name, byte[] src) {
+        if (!active) {
             return;
-        if (abyte0.length > 0x1e8480)
-            return;
-        while (savereq != null)
-            try {
-                Thread.sleep(1L);
-            } catch (Exception _ex) {
-            }
-        savelen = abyte0.length;
-        savebuf = abyte0;
-        for (savereq = String.valueOf(gethash(s)); savereq != null; )
-            try {
-                Thread.sleep(1L);
-            } catch (Exception _ex) {
-            }
+        }
 
+        if (src.length > 2000000) {
+            return;
+        }
+
+        while (savereq != null) {
+            try {
+                Thread.sleep(1L);
+            } catch (Exception _ex) {
+            }
+        }
+
+        savelen = src.length;
+        savebuf = src;
+        for (savereq = String.valueOf(gethash(name)); savereq != null; ) {
+            try {
+                Thread.sleep(1L);
+            } catch (Exception _ex) {
+            }
+        }
     }
 
-    public static synchronized Socket opensocket(int i)
-        throws IOException {
-        for (socketreq = i; socketreq != 0; )
+    public static synchronized Socket opensocket(int port) throws IOException {
+        for (socketreq = port; socketreq != 0; ) {
             try {
                 Thread.sleep(50L);
             } catch (Exception _ex) {
             }
+        }
 
-        if (socket == null)
+        if (socket == null) {
             throw new IOException("could not open socket");
-        else
+        } else {
             return socket;
+        }
     }
 
-    public static synchronized DataInputStream openurl(String s)
-        throws IOException {
-        for (urlreq = s; urlreq != null; )
+    public static synchronized DataInputStream openurl(String url) throws IOException {
+        for (urlreq = url; urlreq != null; ) {
             try {
                 Thread.sleep(50L);
             } catch (Exception _ex) {
             }
+        }
 
-        if (urlstream == null)
-            throw new IOException("could not open: " + s);
-        else
+        if (urlstream == null) {
+            throw new IOException("could not open: " + url);
+        } else {
             return urlstream;
+        }
     }
 
     public static synchronized void dnslookup(String s) {
@@ -339,64 +356,69 @@ public class Signlink
         threadreq = runnable;
     }
 
-    public static synchronized boolean wavesave(byte[] abyte0, int i) {
-        if (i > 0x1e8480)
+    public static synchronized boolean wavesave(byte[] src, int len) {
+        if (len > 2000000) {
             return false;
+        }
+
         if (savereq != null) {
             return false;
-        } else {
-            wavepos = (wavepos + 1) % 5;
-            savelen = i;
-            savebuf = abyte0;
-            waveplay = true;
-            savereq = "sound" + wavepos + ".wav";
-            return true;
         }
+
+        wavepos = (wavepos + 1) % 5;
+        savelen = len;
+        savebuf = src;
+        waveplay = true;
+        savereq = "sound" + wavepos + ".wav";
+        return true;
     }
 
     public static synchronized boolean wavereplay() {
         if (savereq != null) {
             return false;
-        } else {
-            savebuf = null;
-            waveplay = true;
-            savereq = "sound" + wavepos + ".wav";
-            return true;
         }
+
+        savebuf = null;
+        waveplay = true;
+        savereq = "sound" + wavepos + ".wav";
+        return true;
     }
 
-    public static synchronized void midisave(byte[] abyte0, int i) {
-        if (i > 0x1e8480)
+    public static synchronized void midisave(byte[] src, int len) {
+        if (len > 2000000) {
             return;
+        }
+
         if (savereq != null) {
             return;
-        } else {
-            midipos = (midipos + 1) % 5;
-            savelen = i;
-            savebuf = abyte0;
-            midiplay = true;
-            savereq = "jingle" + midipos + ".mid";
-            return;
         }
+
+        midipos = (midipos + 1) % 5;
+        savelen = len;
+        savebuf = src;
+        midiplay = true;
+        savereq = "jingle" + midipos + ".mid";
     }
 
-    public static void reporterror(String s) {
-        if (!reporterror)
-            return;
-        if (!active)
-            return;
-        System.out.println("Error: " + s);
-        try {
-            s = s.replace('@', '_');
-            s = s.replace('&', '_');
-            s = s.replace('#', '_');
-            DataInputStream datainputstream = openurl("reporterror" + 225 + ".cgi?error=" + errorname + " " + s);
-            datainputstream.readLine();
-            datainputstream.close();
-            return;
-        } catch (IOException _ex) {
+    public static void reporterror(String err) {
+        if (!reporterror) {
             return;
         }
+
+        if (!active) {
+            return;
+        }
+
+        System.out.println("Error: " + err);
+//        try {
+//            s = s.replace('@', '_');
+//            s = s.replace('&', '_');
+//            s = s.replace('#', '_');
+//            DataInputStream datainputstream = openurl("reporterror" + 225 + ".cgi?error=" + errorname + " " + s);
+//            datainputstream.readLine();
+//            datainputstream.close();
+//        } catch (IOException _ex) {
+//        }
     }
 
     public Signlink() {
@@ -409,37 +431,45 @@ public class Signlink
     public static boolean active;
     public static int threadliveid;
     public static InetAddress socketip;
+
     public static int socketreq;
-    public static Socket socket = null;
+    public static Socket socket;
+
     public static int threadreqpri = 1;
-    public static Runnable threadreq = null;
-    public static String dnsreq = null;
-    public static String dns = null;
-    public static String loadreq = null;
-    public static byte[] loadbuf = null;
+    public static Runnable threadreq;
+
+    public static String dnsreq;
+    public static String dns;
+
+    public static String loadreq;
+    public static byte[] loadbuf;
+
     public static int savelen;
-    public static String savereq = null;
-    public static byte[] savebuf = null;
-    public static String urlreq = null;
-    public static DataInputStream urlstream = null;
+    public static String savereq;
+    public static byte[] savebuf;
+
+    public static String urlreq;
+    public static DataInputStream urlstream;
+
     public static int looprate = 50;
+
     public static boolean midiplay;
     public static int midipos;
     public static String midi = "none";
     public static int midivol;
     public static int midifade;
-    private Sequencer sequencer = null;
+    private Sequencer sequencer;
+
     public static boolean waveplay;
     public static int wavepos;
     public static String wave = "none";
     public static int wavevol;
-
     enum Position {
         LEFT, RIGHT, NORMAL
     }
-
     private final int EXTERNAL_BUFFER_SIZE = 524288;
-    private Position curPosition;
+    private Position curPosition = Position.NORMAL;
+
     public static boolean reporterror = true;
     public static String errorname = "";
 
